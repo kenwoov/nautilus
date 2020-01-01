5e2e9cb5fdaee26db21043358f508a8b772a43ae
KenWoo <kenwoov@outlook.com>
2020-01-01 22:48:22 +0800

    hello world 2020

--- routes/detail.js
+++ routes/detail.js
@@ -1,14 +1,11 @@
 var express = require('express');
-const uuidv4 = require('uuid/v4')
-
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const uuid = uuidv4();
-  res.render('index', {
-    title: uuidv4().replace(/-/g, '')
-  });
+    res.render('detail', {
+        title: 'Express'
+    });
 });
 
 module.exports = router;
